483b2e3d6be0fa0db987d2b4927f5ef0731ca044
Dutchie3719 <Dutchie3719@gmail.com>
2018-03-14 16:59:58 +0100

    built out matrixces. Now time to write new subfunction to calculate

--- Assignment 2/Assignement_2_Shor_V1.m
+++ Assignment 2/Assignement_2_Shor_V1.m
@@ -3,12 +3,13 @@
 % 4745094
 
 %% Reset MATLAB for New Run
-clear
-clc
+%clear
+%clc
 
 %% Basic Constants
 g = 9.8;                                                                    %% Force of Gravity
 dpla = 1.25;
+dwater = 1;
 %% Preallocating Matricies
 vert = [1,2,6;2,4,6;6,4,5;2,4,3;];
 tria = [0,0;1,3;1,7;5,3;6,0;2,0];
@@ -33,21 +34,33 @@ a3 = 6;
 a4 = 8;
 areatri = [a1;a2;a3;a4];
 totalarea = sum(areatri);
+areatriwet = [a1;a2;a3];
 
 areawet = a1+a2+a3;
 areadry = totalarea - areawet;
 
 %% Centers of Mass
-
 cx = sum(areatri.*centers(:,1))/totalarea;
 cy = sum(areatri.*centers(:,2))/totalarea;
-ShapeCenter = [cx cy];
+ShapeCenter = [cx cy]
 
 cxwet = sum(areatriwet.*centerswet(:,1))/areawet;
 cywet = sum(areatriwet.*centerswet(:,2))/areawet;
-ShapeCenterwet = [cxwet cywet];
+ShapeCenterwet = [cxwet cywet]
 
-%% Function to Optimize
-%inputs centersx(i) centersy(i) 
+%% Solve for Bouyant Force
+fb = g*dwater*areawet;
+fbrem = fb/g;
 
+
+%% Function to Optimize 
+%inputs
+m = zeros(4,1);
+forces = [fb;fbrem];
+x = centersx;
+y = centersy;
+scalars = [1,1,1,1];
+
+Aeq = vertcat(x',scalars);
+Beq = forces;
 
---
+++ Assignment 2/BalanceFcn.m
@@ -0,0 +1,6 @@
+%% %% Modelling Assignement Two
+% Daniel Shor
+% 4745094
+
+function f = balancefcn(m,)
+    f = 
